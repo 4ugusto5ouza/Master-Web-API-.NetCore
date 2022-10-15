b944860652d415b929afd22675a89874ef4abada
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-15 20:56:00 -0300

    :sparkles:feat: add cors e iss configuration

---
+++ CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Extensions/ServicesExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CompanyEmployees.Presentation.WebAPI.Extensions
+{
+    public static class ServicesExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
+            });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) => 
+            services.Configure<IISOptions>(options => { });
+
+    }
+}
--- CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Program.cs
+++ CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Program.cs
@@ -1,17 +1,32 @@
+using CompanyEmployees.Presentation.WebAPI.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
