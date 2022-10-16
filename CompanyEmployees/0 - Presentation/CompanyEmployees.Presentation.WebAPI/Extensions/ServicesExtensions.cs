96aae9444e8cbb8384785b62ea855148edfff35c
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-16 10:38:36 -0300

    :sparkles: add e config NLog para o projeto

--- CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Extensions/ServicesExtensions.cs
+++ CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Extensions/ServicesExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CompanyEmployees.Domain.Interfaces.Logger;
+using CompanyEmployees.Service.Logger;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CompanyEmployees.Presentation.WebAPI.Extensions
@@ -16,6 +18,9 @@ namespace CompanyEmployees.Presentation.WebAPI.Extensions
 
         public static void ConfigureIISIntegration(this IServiceCollection services) => 
             services.Configure<IISOptions>(options => { });
+
+        public static void ConfigureLoggerService(this IServiceCollection services) => 
+            services.AddSingleton<ILoggerManager, LoggerManager>();
 
     }
 }
--- CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Program.cs
+++ CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/Program.cs
@@ -3,12 +3,17 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLog;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 // Add services to the container.
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
 builder.Services.AddControllers();
 
