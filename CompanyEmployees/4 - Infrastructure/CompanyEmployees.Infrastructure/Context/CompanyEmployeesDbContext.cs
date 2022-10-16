5aecbc03ea179c2b798e5b14669d31cccd873324
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-16 11:24:35 -0300

    :sparkles:feat: criado context do projeto

---
+++ CompanyEmployees/4 - Infrastructure/CompanyEmployees.Infrastructure/Context/CompanyEmployeesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CompanyEmployees.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyEmployees.Infrastructure.Context
+{
+    public class CompanyEmployeesDbContext : DbContext
+    {
+        public CompanyEmployeesDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        DbSet<Company>? Companies { get; set; }
+        DbSet<Employee>? Employees { get; set; }
+    }
+}
