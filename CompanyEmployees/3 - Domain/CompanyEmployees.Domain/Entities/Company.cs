a06ec2182c1ad63fc6ff792981c374f7de0e17eb
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-16 11:16:01 -0300

    :sparkles:feat: criado as primeira entidades do sistema

---
+++ CompanyEmployees/3 - Domain/CompanyEmployees.Domain/Entities/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanyEmployees.Domain.Entities
+{
+    public class Company
+    {
+        [Column("Id")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage ="Company name is a required field.")]
+        [MaxLength(60, ErrorMessage ="Maximum lenght for the Name is 60 characters.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Company address is a required field.")]
+        [MaxLength(60, ErrorMessage = "Maximum lenght for the Address is 60 characters.")]
+        public string? Address { get; set; }
+
+        public string? Country { get; set; }
+        public ICollection<Employee>? Employees { get; set; }
+    }
+}
