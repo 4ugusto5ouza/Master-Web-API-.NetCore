28a8e424ff57fd21fa0974a72e959c93455ad54b
Samuel Souza <augusto_souza@edu.unifil.br>
2022-10-16 11:37:05 -0300

    :sparkles:feat: criado context faxtory para gestão dos contextos

---
+++ CompanyEmployees/0 - Presentation/CompanyEmployees.Presentation.WebAPI/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using CompanyEmployees.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace CompanyEmployees.Presentation.WebAPI.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<CompanyEmployeesDbContext>
+    {
+        public CompanyEmployeesDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<CompanyEmployeesDbContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlConnection")); 
+            return new CompanyEmployeesDbContext(builder.Options);
+        }
+    }
+}
